a6031a437e46d24737c273d4ae4872e120fd4ba3
Stefano Nava <stefano.nava@sncegroup.ch>
2020-03-11 11:19:29 +0100

    Updated script in order to fix minor issue

--- index.js
+++ index.js
@@ -1,28 +1,32 @@
 const fs = require('fs');
 const zlib = require('zlib');
-var execCommand = require("./helper/exec-command");
 
 const runCompression = function (dirs) {
   dirs.forEach(dir => {
     fs.readdirSync(dir).forEach(file => {
       if (file.endsWith('.js') || file.endsWith('.css') || file.endsWith('.html')) {
         var name = file.replace('.', '_original.');
-        console.log('Compress file: ' + file);
-        var command = "mv " + dir + '/' + file + " " + dir + '/' + name;
-        execCommand(command);
-        setTimeout(function () {
+        fs.rename(dir + '/' + file, dir + '/' + name, (err) => {
+          if (err) throw err;
           const fileContents = fs.createReadStream(dir + '/' + name);
-          const writeStream = fs.createWriteStream(dir + '/' + file);
-          const zip = zlib.createGzip();
-          fileContents
-            .pipe(zip)
-            .on('error', err => console.error(err))
-            .pipe(writeStream)
-            .on('error', err => console.error(err));
-          fs.unlinkSync(dir + '/' + name);
-        }, 500);
+          fs.exists(dir + '/' + name, (exists) => {
+            const fileContents = fs.createReadStream(dir + '/' + name);
+            const writeStream = fs.createWriteStream(dir + '/' + file);
+            const zip = zlib.createGzip();
+            setTimeout(function () {
+              console.log('START compress file: ' + file);
+              fileContents
+                .pipe(zip)
+                .on('error', err => console.error(err))
+                .pipe(writeStream)
+                .on('error', err => console.error(err));
+              fs.unlinkSync(dir + '/' + name);
+              console.log('END compress file: ' + file);
+            }, 500);
+          });
+        });
       }
-    })
+    });
   });
 };
 
